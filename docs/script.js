ee6a4e3830f568d57cb70582653df55b73e4cd67
Adam <69228473+kartibrown@users.noreply.github.com>
2026-05-19 13:56:31 +0200

    Initialize canvas and draw a red rectangle

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,8 @@
+const canvas = document.getElementById("world");
+const ctx = canvas.getContext("2d");
 
+canvas.width = 800;
+canvas.height = 600;
+
+ctx.fillStyle = "red";
+ctx.fillRect(100, 100, 50, 50);
