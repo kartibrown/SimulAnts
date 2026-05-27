6fdb6991bc501957e487fd280ca1baa40c95ff8f
Karti <tretris57@gmail.com>
2026-05-27 02:56:40 +0200

    removed duplicated code

--- src/main/java/com/kartibrown/simulants/ant/WorkerAnt.java
+++ src/main/java/com/kartibrown/simulants/ant/WorkerAnt.java
@@ -149,21 +149,6 @@ public class WorkerAnt extends Ant
 			energy--;
 	}
 
-	@Override
-	public void move(final World world)
-	{
-		final int xMove = rng.nextBoolean() ? 1 : -1;
-		final int yMove = rng.nextBoolean() ? 1 : -1;
-
-		pos.setX(pos.getX() + (rng.nextBoolean() ? xMove : 0));
-		pos.setY(pos.getY() + (rng.nextBoolean() ? yMove : 0));
-
-		pos.setX(Math.max(0, Math.min(pos.getX(), world.getSizeX() - 1)));
-		pos.setY(Math.max(0, Math.min(pos.getY(), world.getSizeY() - 1)));
-
-		this.energy--;
-	}
-
 	protected final void storeFoodAt(final Colony colony)
 	{
 		colony.storeAll(inventory);
